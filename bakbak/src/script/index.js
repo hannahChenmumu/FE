4338b408a8728d765e88608a0b954f25f6ab33c3
hannah <hannahcjh@163.com>
2017-12-27 23:38:51 +0800

    前奏动画

--- bakbak/src/script/index.js
+++ bakbak/src/script/index.js
@@ -6,6 +6,6 @@ new Vue({
     },
     methods: {},
     mounted(){
-        this.elems = `<div class="elem1-1"></div><div class="elem1-2"></div><div class="elem1-3"></div>`
+        this.elems = '<div class="elem1-1"></div><div class="elem1-2"></div><div class="elem1-3"></div><div class="elem1-4"></div><div class="elem1-5"></div><div class="elem1-6"></div><div class="elem1-7"></div><div class="elem1-8"></div>'
     }
 })

6fa4054fa47cfc63cca8d17d0180e2f94dc325f7
hannah <hannahcjh@163.com>
2017-12-30 01:13:15 +0800

    加了浏览器内点击触发，微信内未搞定

--- bakbak/src/script/index.js
+++ bakbak/src/script/index.js
@@ -1,48 +1,77 @@
+var $audio = $('#audio')[0]
+
 new Vue({
     el: '#app',
     data: {
         loadingMode: false, // true为音频加载完成
         scene: 'scene1',
-        elems: ''
-    },
-    watch: {
-
+        elems: '',
+        statusDl: false, // 音频准备就绪
+        statusGo: false
     },
     methods: {
         // TODO 计算某些元素满屏高度
-        calcuHeight(dom){
+        calcuHeight: function(dom){
             var pageHeight = $(window).height()
             console.log(pageHeight)
             $('#app>div').css('height', pageHeight)
             $('.scene1 .elem0-7').css('height', pageHeight)
-        }
+        },
+        startAnimation: function(){
+            this.statusTap = true
+        },
+        herewego: function(){
+            this.statusGo = true
+            // $audio.src = 'http://192.168.1.15:8000/media/guiyangtanqi-320.mp3'
+            $audio.play()
+            // setTimeout(function(){
+
+                this.elems = '<div class="elem0-1"></div><div class="elem0-2"></div><div class="elem0-3"></div><div class="elem0-4"></div><div class="elem0-5"></div><div class="elem0-6"></div><div class="elem0-7"></div><div class="elem0-8"></div><div class="elem1-1"></div><div class="elem1-2"></div><div class="elem1-3"></div><div class="elem1-4"></div>'
+
+            // }, 6000)
+        },
+    },
+    watch:{
     },
     mounted() {
-        this.calcuHeight()
+        var _self = this
 
-        var audio = document.getElementById('audio')
+        _self.calcuHeight()
 
-        if(this.loadingMode){
-            this.elems = '<div class="loading">LOADING...</div>'
+        $audio.load()
 
-            // 移动端触摸播放音频
-            // TODO 做loading，完成后加载动画和音频
-            audio.addEventListener("canplay", function() {
-                this.elems = '<div class="loading">Tap to start!</div>'
-                $('html').one('touchstart', function () {
-                    audio.play()
-                    this.already = true
-                })
-            })
-            if (this.already) {
-                this.elems = '<div class="elem0-1"></div><div class="elem0-2"></div><div class="elem0-3"></div><div class="elem0-4"></div><div class="elem0-5"></div><div class="elem0-6"></div><div class="elem0-7"></div><div class="elem0-8"></div><div class="elem1-1"></div><div class="elem1-2"></div><div class="elem1-3"></div><div class="elem1-4"></div>'
+        self.setInterval(function(){
+            if ($audio.readyState ==4 ){
+                _self.statusDl = true
             }
+        },50)
+
+        document.addEventListener("touchstart", function (e) {
+            // if(e.target.attr('id')=='start'){
+                if(_self.statusDl)
+                    _self.herewego()
+            // }
+        })
+        document.removeEventListener("touchstart", function(){})
+        // 微信
+        document.addEventListener("WeixinJSBridgeReady", function () {
+            $audio.play();
+        }, false);
+        document.addEventListener('YixinJSBridgeReady', function() {
+            $audio.play();
+        }, false);
+
+
+        /*if(_self.loadingMode){
+            // _self.elems = '<div class="loading">LOADING...</div>'
+            _self.elems = '<div class="loading" id="start" v-on:click="startAnimation">Tap to start</div>'
+
         }else{
-            audio.play();
+            $audio.play();
             $(document).one("touchstart",function() {
                 audio.play()
             })
-            this.elems = '<div class="elem0-1"></div><div class="elem0-2"></div><div class="elem0-3"></div><div class="elem0-4"></div><div class="elem0-5"></div><div class="elem0-6"></div><div class="elem0-7"></div><div class="elem0-8"></div><div class="elem1-1"></div><div class="elem1-2"></div><div class="elem1-3"></div><div class="elem1-4"></div>'
-        }
+            _self.elems = '<div class="elem0-1"></div><div class="elem0-2"></div><div class="elem0-3"></div><div class="elem0-4"></div><div class="elem0-5"></div><div class="elem0-6"></div><div class="elem0-7"></div><div class="elem0-8"></div><div class="elem1-1"></div><div class="elem1-2"></div><div class="elem1-3"></div><div class="elem1-4"></div>'
+        }*/
     }
 })

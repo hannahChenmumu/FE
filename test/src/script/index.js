c8f4dc7e8f0c0f7e5e63cc16941e7d6d435d7fad
hannah <hannahcjh@163.com>
2017-12-30 23:30:52 +0800

    testAnimation

--- test/src/script/index.js
+++ test/src/script/index.js
@@ -1,15 +1,7 @@
 $(function(){
-    var $audio = $('#audio')[0]
 
     var pageHeight = $(window).height()
     console.log(pageHeight)
     $('#app>div').css('height', pageHeight)
 
-    // 微信
-    document.addEventListener("WeixinJSBridgeReady", function () {
-        $audio.play();
-    }, false);
-    document.addEventListener('YixinJSBridgeReady', function() {
-        $audio.play();
-    }, false);
 })
